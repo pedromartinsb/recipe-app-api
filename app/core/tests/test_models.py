9990887065a858e7c52d8f1356b5eb2897a4649a
Pedro Campos <pedro.campos@startaideia.com.br>
2021-01-01 14:01:00 -0300

    Added custom model

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -8,8 +8,8 @@ class ModelTest(TestCase):
         email = 'test@example.com'
         password = 'Testpass123'
         user = get_user_model().objects.create_user(
-            email=email,
-            password=password
+            email = email,
+            password = password
         )
 
         self.assertEqual(user.email, email)
