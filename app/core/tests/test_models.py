e829f3a6c8ea3321b164f4fc4de529345a5394a4
Pedro Campos <pedro.campos@startaideia.com.br>
2021-01-02 01:33:51 -0300

    Fix issue on tests

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,5 +1,7 @@
+
 from django.test import TestCase
 from django.contrib.auth import get_user_model
+
 
 class ModelTest(TestCase):
 
@@ -8,8 +10,8 @@ class ModelTest(TestCase):
         email = 'test@example.com'
         password = 'Testpass123'
         user = get_user_model().objects.create_user(
-            email = email,
-            password = password
+            email=email,
+            password=password
         )
 
         self.assertEqual(user.email, email)
